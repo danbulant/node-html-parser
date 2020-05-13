0d81bd6f1bf6e57b45ec82c01b748e76b523b89c
Daniel Bulant <admin@danbulant.eu>
2020-05-13 11:56:33 +0200

    Add extensions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { default as CommentNode } from './nodes/comment';
-export { default as HTMLElement, parse, parse as default, Options } from './nodes/html';
-export { default as Node } from './nodes/node';
-export { default as TextNode } from './nodes/text';
-export { default as NodeType } from './nodes/type';
+export { default as CommentNode } from './nodes/comment.ts';
+export { default as HTMLElement, parse, parse as default, Options } from './nodes/html.ts';
+export { default as Node } from './nodes/node.ts';
+export { default as TextNode } from './nodes/text.ts';
+export { default as NodeType } from './nodes/type.ts';
